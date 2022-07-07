abcd8281d5546accbbd38a6ac2ba1934f0406e9c
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-07-07 21:14:26 +0100

    Added scrollable card layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,21 @@
 <x-layout>
-    <div>
-        <x-navbar></x-navbar>
+    <x-navbar></x-navbar>
+    <div class="container">
+        
         <div class="search-lg-centred">
             <h1>Find your tribe</h1>
             <x-search></x-search>
+        </div>
+        <div class="down-arrow">
+            <h1>></h1>
+        </div>
+    </div>
+    <div class="container">
+        <div class="card-container">
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
         </div>
     </div>
 </x-layout>
