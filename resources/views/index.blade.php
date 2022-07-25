144de1643b45b7c17c17cd735dd3100744eccd88
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-07-25 19:31:12 +0100

    Home page styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,16 @@
 <x-layout>
-    <x-navbar></x-navbar>
-    <div class="container">
-        
-        <div class="search-lg-centred">
-            <h1>Find your tribe</h1>
-            <x-search></x-search>
-        </div>
-        <div class="down-arrow">
-            <h1>></h1>
+    <div class="background-img">
+        <div class="background-img-fade">
+            <x-navbar></x-navbar>
+            <div class="container">
+                <div class="search-lg-centred">
+                    <h1>Find your tribe</h1>
+                    <x-search></x-search>
+                </div>
+                <div class="down-arrow">
+                    <h1>></h1>
+                </div>
+            </div>
         </div>
     </div>
     <div class="container">
