1d141ef5da96605648cf98889f72705dff8449c2
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-07-31 18:43:54 +0100

    Details page styling

--- resources/views/meetups/show.blade.php
+++ resources/views/meetups/show.blade.php
@@ -8,8 +8,24 @@
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, a accusamus architecto velit necessitatibus asperiores facere itaque fugiat maxime numquam debitis repudiandae illo, pariatur similique dolores dolore tenetur iusto aut.</p>
             </div>
             <div class="meetup-details-second">
-                <div class="meetup-details-group">London Fixed Gear Society</div>
-                <div class="meetup-details-info">Sat 27 Aug - 4:00PM</div>
+                <div class="meetup-group-card">
+                    <img src="" alt="" class="meetup-details-group-img">
+                    <div class="meetup-details-group">
+                        <h4>Hosted by</h4>
+                        <h3>London Fixed Gear Society</h3>
+                    </div>
+                </div>
+                <ul class="meetup-details-info">
+                    <li>London</li>
+                    <li>Sat 27 Aug - 4:00PM</li> 
+                    <li>
+                        <ul class="meetup-tags-list">
+                            <li class="meetup-tag"><a href="/">fixed</a></li>
+                            <li class="meetup-tag"><a href="/">london</a></li>
+                            <li class="meetup-tag"><a href="/">beginner</a></li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
