fc812723f9b3df69e7352a1d8a19834424cdbbc4
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-12-06 20:55:33 +0000

    Added icons to Show Meetup page

--- resources/views/meetups/show.blade.php
+++ resources/views/meetups/show.blade.php
@@ -3,11 +3,12 @@
     <div class="small-container">
         <div class="meetup-details-container">
             <div class="meetup-details-first">
-                <h1>{{$meetup['name']}}</h1>
+                <h1 class="hidden-on-mobile">{{$meetup['name']}}</h1>
                 <div class="meetup-details-image" style="background:url({{asset('storage/' . $meetup->image)}}) no-repeat center/cover"></div>
                 <p>{{$meetup['description']}}</p>
             </div>
             <div class="meetup-details-second">
+                <h1 class="hidden-on-desktop">{{$meetup['name']}}</h1>
                 <div class="meetup-group-card">
                     <img src="{{asset('img/user.png')}}" alt="" class="meetup-details-group-img">
                     <div class="meetup-details-group">
@@ -16,9 +17,9 @@
                     </div>
                 </div>
                 <ul class="meetup-details-info">
-                    <li>{{$meetup['location']}}</li>
-                    <li>{{ \Carbon\Carbon::parse($meetup->date)->format('j F, Y')}}</li> 
-                    <li>{{$meetup['time']}}</li> 
+                    <li><img class="meetup-details-icon" src="{{asset('img/location-dot-solid.svg')}}" alt="">{{$meetup['location']}}</li>
+                    <li><img class="meetup-details-icon" src="{{asset('img/calendar-regular.svg')}}" alt="">{{ \Carbon\Carbon::parse($meetup->date)->format('j F, Y')}}</li> 
+                    <li><img class="meetup-details-icon" src="{{asset('img/clock-solid.svg')}}" alt="">{{$meetup['time']}}</li> 
                 </ul>
             </div>
         </div>
