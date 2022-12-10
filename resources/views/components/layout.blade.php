66a1c587ac72d382e510ba9dedf7372724696861
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2022-12-10 20:04:27 +0000

    Added Alpine js for flash messages

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,19 +4,26 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/css/app.css">
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <title>BikeTribe | Cycling meetups & groups</title>
     </head>
     <body>
         <x-navbar></x-navbar>
         
         @if(session()->has('success'))
-            <div class="message-container message-success">
+            <div x-data="{ show:true }" 
+                x-init="setTimeout(() => show = false, 3000)"
+                x-show="show"
+                class="message-container message-success">
                 {{session('success')}}
             </div>
         @endif
 
         @if(session()->has('failure'))
-            <div class="message-container message-fail">
+            <div x-data="{ show:true }" 
+                x-init="setTimeout(() => show = false, 3000)"
+                x-show="show"
+                class="message-container message-fail">
                 {{session('failure')}}
             </div>
         @endif
